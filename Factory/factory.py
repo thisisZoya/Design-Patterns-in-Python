58fb24bed8d6c2b9a6d30e9dd30df354bd6f6464
thisisZoya <zoya.zq.210@gmail.com>
2025-07-21 17:21:44 +0400

    Add Factory pattern

---
+++ Factory/factory.py
@@ -0,0 +1,47 @@
+from abc import ABC, abstractmethod
+
+
+class File(ABC):  # creator
+    def __init__(self, file):
+        self.file = file
+
+    @abstractmethod
+    def make(self):
+        pass
+
+    def call_edit(self):
+        product = self.make()
+        result = product.edit(self.file)
+        return result
+
+
+class JsonFile(File):  # creator
+    def make(self):
+        return Json()
+
+
+class XmlFile(File):  # creator
+    def make(self):
+        return Xml()
+
+
+class Json:  # product
+    def edit(self, file):
+        return f'Working on {file} Json...'
+
+
+class Xml:  # product
+    def edit(self, file):
+        return f'Working on {file} Xml...'
+
+
+def client(file, format):  # client
+    formats = {
+        'json': JsonFile,
+        'xml': XmlFile
+    }
+    result = formats[format](file)
+    return result.call_edit()
+
+
+print(client('show', 'xml'))
